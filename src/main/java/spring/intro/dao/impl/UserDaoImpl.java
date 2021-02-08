afcefccaaeb0c173f0acd6a42b58925c9ac36adc
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-08 23:41:21 +0200

    added package model and class User, added package dao added interface UserDao and class UserDaoImpl, added package service, added interface UserService and class UserServiceImpl, added package exception and class DataProcessingException, added package config and class AppConfig, added class Main for testing implementation UserServiceImpl.

---
+++ src/main/java/spring/intro/dao/impl/UserDaoImpl.java
@@ -0,0 +1,55 @@
+package spring.intro.dao.impl;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import spring.intro.dao.UserDao;
+import spring.intro.exception.DataProcessingException;
+import spring.intro.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save user "
+                    + user + " to table. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT u FROM User u", User.class)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error with get list of users from table", e);
+        }
+    }
+}
---
+++ src/main/java/spring/intro/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package spring.intro.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.intro.dao.UserDao;
+import spring.intro.model.User;
+import spring.intro.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+}
